321c72fc9bc78d4d768e51bd6d64b914664880ff
lvivduncan <lvivduncan@gmail.com>
2021-08-27 14:34:22 +0300

    todo: new slider

--- js/script.js
+++ js/script.js
@@ -82,6 +82,85 @@
     }
 }
 
+{
+    // scroll
+    const scroll = document.querySelectorAll('.levus-horizontal-scroll');
+
+    scroll.forEach(item => {
+        const ul = item.querySelector('ul');
+
+        // elements
+        let li = ul.querySelectorAll('li');
+
+        // if less than 4, cloned 
+        if (li.length <= 4) {
+
+            // cloned and append elements
+            li.forEach(element => ul.append(element.cloneNode(true)));
+
+            // new nodelist
+            li = item.querySelectorAll('li');
+        }
+
+        item.innerHTML += '<div class="levus-nav"><span class="left"></span><span class="right"></span></div>';
+
+        item.addEventListener('click', e => {
+            const ul = item.querySelector('ul');
+            if (e.target.className == 'left') {
+
+                // move last element
+                const last = ul.lastElementChild;
+                ul.prepend(last);
+
+                // destroy transition
+                ul.style.transition = 'none';
+                ul.classList.add('to-right');
+                setTimeout(() => {
+                    ul.classList.remove('to-right');
+                    ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+
+        item.addEventListener('click', e => {
+            const ul = item.querySelector('ul');
+            if (e.target.className == 'right') {
+
+                // move first element
+                const first = ul.firstElementChild;
+                ul.append(first);
+
+                // destroy transition
+                ul.style.transition = 'none';
+                ul.classList.add('to-left');
+                setTimeout(() => {
+                    ul.classList.remove('to-left');
+                    ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+
+        setInterval(() => {
+            const ul = item.querySelector('ul');
+
+            // move first element
+            const first = ul.firstElementChild;
+            ul.append(first);
+
+            // destroy transition
+            ul.style.transition = 'none';
+            ul.classList.add('to-left');
+
+            setTimeout(() => {
+                ul.classList.remove('to-left');
+                ul.style.transition = '.5s';
+            }, 50);
+         
+        }, 3500);
+        
+    });
+}
+
 
 let mymap = L.map('map-place').setView([49.899444, 23.943889], 15);
 
